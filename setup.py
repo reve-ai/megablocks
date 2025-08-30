54cf3d9b58f8202406ea24729efa63d9f654433b
Michael Gharbi <1612767+mgharbi@users.noreply.github.com>
2025-08-30 10:16:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ classifiers = [
 ]
 
 install_requires = [
-    'numpy>=1.21.5,<2.1.0',
+    # 'numpy>=1.21.5,<2.1.0',
+    'numpy>=1.21.5,  # NOTE(mgharbi): remove numpy v2 constraint.
     'packaging>=21.3.0,<24.2',
     'torch>=2.7.0,<2.7.1',
     'stanford-stk==0.7.1',
